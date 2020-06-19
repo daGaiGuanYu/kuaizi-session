e47291ff5e3152fd536a3c3865160280e6499df4
关于 <372301467@qq.com>
2020-06-19 10:57:52 +0800

    update

--- src/get-session/index.js
+++ src/get-session/index.js
@@ -1,7 +1,18 @@
+const Cookie = require('cookie')
+
 const { read } = require('../persistence/read/index')
+const cfg = require('../cfg/index')
 
 const data = read()
 
 module.exports = function(req, res){
-  
+  let token = getTokenFromRequest(req)
+  if(!token||data[token])
+    data[token] = {}
+  return data[token]
+}
+
+function getTokenFromRequest(req){
+  let cookieObj = Cookie.parse(req.headers.cookie||'')
+  return cookieObj[cfg.TokenName]
 }
