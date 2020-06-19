46dedeed071c9c0e16c886c3d01cef2b176695fa
关于 <372301467@qq.com>
2020-06-19 12:15:26 +0800

    完善 example

--- example/src/index.js
+++ example/src/index.js
@@ -7,7 +7,11 @@ Server.start()
 
 HandleRequest.get('/', ctx => {
   let session = getSession(ctx.req, ctx.res)
-  console.log(session)
-  session.name = 1
+  session.name = '小明'
   return 1
+})
+
+HandleRequest.get('/who', ctx => {
+  let session = getSession(ctx.req, ctx.res)
+  return session.name || '不认识'
 })
