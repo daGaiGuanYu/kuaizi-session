41a354f265664375c560871653b98bde6f74fd1e
关于 <372301467@qq.com>
2020-06-19 11:17:10 +0800

    update

--- src/get-session/index.js
+++ src/get-session/index.js
@@ -1,4 +1,5 @@
 const Cookie = require('cookie')
+const getUniqueString = require('simple-unique-string')
 
 const { read } = require('../persistence/read/index')
 const cfg = require('../cfg/index')
@@ -7,12 +8,25 @@ const data = read()
 
 module.exports = function(req, res){
   let token = getTokenFromRequest(req)
-  if(!token||data[token])
-    data[token] = {}
-  return data[token]
+  let result // 先用 result 暂存 session 对象，否则，每取一次 data[token] 会触发 proxy 的 getter
+  
+  if(!token) // 连 token 都没有
+    token = getUniqueString()
+  if(!data[token]) // token 过期
+    result = data[token] = {}
+  else // token 正常
+    result = data[token]
+
+  putCookie(res, token) // 放\更新 前端 cookie
+  return result
 }
 
 function getTokenFromRequest(req){
   let cookieObj = Cookie.parse(req.headers.cookie||'')
   return cookieObj[cfg.TokenName]
+}
+function putCookie(res, token){
+  let expireAt = new Date().getTime() + cfg.Warranty
+  expireAt = new Date(expireAt).toUTCString()
+  res.setHeader('Set-Cookie', `${cfg.TokenName}=${token};path=/;httpOnly;expires=${expireAt}`)
 }
