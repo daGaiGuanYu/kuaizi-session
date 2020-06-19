e93eee356d239ee9455f75a8bf61f360bd7b8c9d
关于 <372301467@qq.com>
2020-06-19 11:53:21 +0800

    update

--- src/persistence/check.js
+++ src/persistence/check.js
@@ -4,12 +4,13 @@ const cfg = require('../cfg/index')
 module.exports = function(data){
   let now = new Date()
   for(let token in data){
-    let timeLeft = cfg.Warranty - (now - data[token].startTime) // 剩余时间 = 总时间 减 己过多长时间  
+    data[token].startTime = new Date(data[token].startTime)
+    let timeLeft = cfg.Warranty - (now - data[token].startTime) // 剩余时间 = 总时间 减 己过多长时间
     if(timeLeft < 1)
       delete data[token]
-    data.startTime = now
-    data.timeoutId = setTimeout(function(){
-      delete data[token]
-    }, timeLeft)
+    else
+      data[token].timeoutId = setTimeout(function(){
+        delete data[token]
+      }, timeLeft)
   }
 }
